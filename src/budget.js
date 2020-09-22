786b253dd4d1ba69f32ac6df6ae9236132f52234
Rkebede <rutht.kebede@gmail.com>
2020-09-22 17:46:41 -0500

    adds functions to budget class

--- src/budget.js
+++ src/budget.js
@@ -5,6 +5,17 @@ class Budget {
     this.expenses = []
   };
 
+  totalExpenses(){
+    let expensesAmount = this.expenses.values
+    expensesAmount.reduce((acc, expense) => {
+      return acc + expense
+    }, 0)
+  }
+
+  expensesMinusIncome(){
+   return this.income - this.totalExpenses 
+  }
+
 }
 
 
