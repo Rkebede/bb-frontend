adcef6202c19b56ad26a33cc421fad5702374460
Rkebede <rutht.kebede@gmail.com>
2021-02-17 16:52:49 -0600

    adds search bar

--- src/expenseAccordion.js
+++ src/expenseAccordion.js
@@ -16,6 +16,34 @@ class ExpenseAccordion {
     expenseAccordion.appendChild(this.title)
     expenseAccordion.appendChild(accordionContent)
     accordionContent.appendChild(this.expenseTable.render())
+    let search = document.createElement('input')
+    search.setAttribute('id', 'search')
+    search.setAttribute('placeholder', 'Search by Amount')
+    let saveSearch = document.createElement('button')
+    saveSearch.setAttribute('id', 'saveSearch')
+    saveSearch.innerText = "Search"
+    
+    saveSearch.addEventListener('click', (e)=>{
+      let searchInput = e.target.previousElementSibling.value
+      // for(let x in Expense.all){ 
+      //   if (Expense.all[x].amount > searchInput){
+      //     Expense.all[x].expenseCell.remove()
+      //   }
+      // }
+
+      //  let filtered = Object.values(Expense.all).filter((expense) =>{ 
+      //     expense.expenseCell.remove()
+      //     return expense.amount <= searchInput
+      //   })
+      //   filtered.forEach((expense) => { new ExpenseCell(expense)})
+
+      Object.values(Expense.all).forEach((expense) => { 
+        if (expense.amount > searchInput){ expense.expenseCell.remove()}
+       })
+
+    })
+    expenseAccordion.appendChild(search)
+    expenseAccordion.appendChild(saveSearch)
   }
 
   appendAllExpenses() {
